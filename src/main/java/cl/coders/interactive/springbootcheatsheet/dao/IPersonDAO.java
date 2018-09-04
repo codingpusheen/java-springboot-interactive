6de95c8d58958bc8854a4ad63f4846a9098815bf
Esteban Peralta Galarce <estebanalejandro@gmail.com>
2018-09-04 02:45:10 -0300

    first code push

---
+++ src/main/java/cl/coders/interactive/springbootcheatsheet/dao/IPersonDAO.java
@@ -0,0 +1,10 @@
+package cl.coders.interactive.springbootcheatsheet.dao;
+
+import java.util.List;
+
+import cl.coders.interactive.springbootcheatsheet.entity.Person;
+
+public interface IPersonDAO {
+
+	  List<Person> getAllArticles();
+}
---
+++ src/main/java/cl/coders/interactive/springbootcheatsheet/dao/PersonDAO.java
@@ -0,0 +1,25 @@
+package cl.coders.interactive.springbootcheatsheet.dao;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import cl.coders.interactive.springbootcheatsheet.entity.Person;
+
+@Transactional
+@Repository
+public class PersonDAO implements IPersonDAO{
+
+	@PersistenceContext	
+	private EntityManager entityManager;
+	
+	@Override
+	public List<Person> getAllArticles() {
+		String hql = "FROM Person as p ORDER BY p.personId";
+		return (List<Person>) entityManager.createQuery(hql).getResultList();
+	}
+
+}
