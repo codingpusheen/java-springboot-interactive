457b3c1ee70192715fc6d616e8ce5cfc488e3a8e
Esteban Peralta Galarce <estebanalejandro@gmail.com>
2019-03-31 21:53:52 -0300

    DAO crud operations

--- src/main/java/cl/codefluffylearning/interactive/springbootcheatsheet/dao/ICourseDAO.java
+++ src/main/java/cl/codefluffylearning/interactive/springbootcheatsheet/dao/ICourseDAO.java
@@ -7,4 +7,8 @@ import cl.codefluffylearning.interactive.springbootcheatsheet.entity.Course;
 public interface ICourseDAO {
 
 	  List<Course> getAllCourses();
+	  String createCourse(Course course);
+	  Course findCourse(Long courseId);
+	  String updateCourse(Course course);
+	  String deleteCourse(Long courseId);
 }
